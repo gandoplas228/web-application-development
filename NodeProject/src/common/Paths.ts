e0eba88bafae00a21065a5d3cc4c6beda206b1db
gandoplas228 <chudovaea@mer.ci.nsu.ru>
2024-10-25 16:11:03 +0700

    laba3

---
+++ NodeProject/src/common/Paths.ts
@@ -0,0 +1,22 @@
+/**
+ * Express router paths go here.
+ */
+
+
+export default {
+  Base: '/api',
+  Users: {
+    Base: '/users',
+    Get: '/all',
+    Add: '/add',
+    Update: '/update',
+    Delete: '/delete/:id',
+  },
+  Examples: {
+    Base: '/examples',
+    Get: '/example/all',
+    Add: '/example/add',
+    Update: '/example/update',
+    Delete: '/example/delete/:id',
+  },
+} as const;
